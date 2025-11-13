445ae66525af58945712fb82fd93588a09a611ab
Daniel dos Santos C <danielsantos@sysfar.com.br>
2025-11-13 20:31:33 -0300

    feat: Implementa fontes e estrutura de cores do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import { SafeAreaView } from "react-native-safe-area-context";
-import { NavigationContainer } from "@react-navigation/native";
+import { Inter_800ExtraBold, Inter_400Regular_Italic, Inter_400Regular, useFonts } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
 import { AppRoutes } from "./Routes";
+import { useEffect } from 'react';
 
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+    const [loaded, err] = useFonts({
+        extraBold: Inter_800ExtraBold,
+        regularItalic: Inter_400Regular_Italic,
+        regular: Inter_400Regular
+    });
+
+    useEffect(() => {
+        if (loaded || err) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded, err]);
+
+
+    if (!loaded && err) {
+        return null;
+    }
+
     return (
         <AppRoutes />
     );
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,12 @@
 import { Button, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { HomeScreenNavigationProp } from "../navigation/types";
-import { SafeAreaView } from "react-native-safe-area-context";
-
 
 export function Home() {
     const navigation = useNavigation<HomeScreenNavigationProp>();
     return (
         <>
-            <Text>Home Screen</Text>
+            <Text style={{ fontFamily: 'extraBold' }}>Home Screen</Text>
             <Button title="Go to Details" onPress={() => navigation.navigate('details', { rate: 5 })}></Button>
             <Button title="Set User Name" onPress={() => navigation.navigate('setUserName')}></Button>
         </>
